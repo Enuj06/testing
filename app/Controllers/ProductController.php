6b219e5fe6a1f4d370125df47749be56f4c9d3bd
Enuj06 <junebalaibo03@gmail.com>
2023-09-24 22:36:24 +0800

    main

--- app/Controllers/ProductController.php
+++ app/Controllers/ProductController.php
@@ -3,19 +3,62 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ProductModel;
 
 class ProductController extends BaseController
 {
+    private $product;
+    public function __construct()
+    {
+        $this->product = new ProductModel();
+        
+    }
+
+    public function delete($id)
+    {
+        $this->product->delete($id);
+        return redirect()->to('/product');
+    }
+
+    public function edit($id)
+    {
+        $data =[
+            'product' => $this->product->findAll(),
+            'pro' => $this->product->where('id', $id)->first(),
+        ];
+            return view('products', $data);
+
+        
+     /*   $this->product->update($id);
+        return redirect()->to('/product');   */
+    }
+
+    public function save()
+    {
+        $data = [
+            
+            'code' => $this->request->getVar('code'),
+            'name' => $this->request->getVar('name'),
+            'quantity' => $this->request->getVar('quantity')
+        ];
+        $this->product->save($data);
+        return redirect()->to('/product');
+    }
+    public function index()
+    {
+        $data['products'] = $this->product->findAll();
+        return view('main', $data);
+    }
     public function product($product)
     {
         echo $product;
     }
     public function Enuj06()
     {
-        return view('products');
+       $data['product'] = $this->product->findAll();
+       return view('products', $data);
     }
-    public function index()
-    {
-        //
-    }
+    
+
 }
+
--- app/Views/products.php
+++ app/Views/products.php
@@ -5,7 +5,36 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
 </head>
+<?php ?>
 <body>
+    <form action="/save" method="post">
+            <label>Code</label>
+            <input type="text" name="code" placeholder="Code" value="<?=$pro['code']?>">
+            <br>
+            <label>Name</label>
+            <input type="text" name="name" placeholder="Name" value="<?=$pro['name']?>">
+            <br>
+            <label>Quantity</label>
+            <input type="text" name="quantity" placeholder="Quantity" value="<?=$pro['quantity']?>">
+            <br>
+            <input type="submit" value="save">
+    </form>
     <h1 style="text-align: center">Product Listing</h1>
+    <table border="1">
+        <tr>
+            <th>Code</th>
+            <th>Name</th>
+            <th>Quantity</th>
+            <th colspan="2">Action</th>
+        </tr>
+        <?php foreach ($product as $pr): ?>
+            <tr>
+            <td><?= $pr['code'] ?></td>
+            <td><?= $pr['name'] ?></td>
+            <td><?= $pr['quantity'] ?></td>
+            <td><a href="/delete/<?= $pr['id'] ?>">Delete</a> || <a href="/update/<?= $pr['id'] ?>">Update</a> </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
 </body>
 </html>
